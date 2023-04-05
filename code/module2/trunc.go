5fb31e1158563229509c66a0a3f842e607e35575
acanete <alba.canete@bsc.es>
2023-04-05 10:58:27 +0200

    change vars to 32b

--- code/module2/trunc.go
+++ code/module2/trunc.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	var numF float64
+	var numF float32
 
 	fmt.Println("Enter a float number: ")
 
@@ -15,7 +15,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var numI int
-	numI = int(numF)
+	var numI int32
+	numI = int32(numF)
 	fmt.Printf("Its truncated value is %d \n", numI)
 }
